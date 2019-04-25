f64bf7ae851c3cf40059c20fafea2a459a83c431
Rakshit Kapoor <500067642@stu.upes.ac.in>
2019-04-25 23:44:13 +0530

    Code added

--- system.cpp
+++ system.cpp
@@ -32,3 +32,40 @@ public:
         puts(pass);
     }
 };
+class detail
+{
+public:
+    int tno;
+    char tname[100];
+
+    char bp[100];
+    char dest[100];
+    int c1,c1fare;
+    int c2,c2fare;
+    int d,m,y;
+    void getdetail()
+    {
+        cout<<"Enter the details as follows\n";
+        cout<<"Train no:";
+        cin>>tno;
+        cout<<"Train name:";
+        gets(tname);
+        cout<<"Boarding point:";
+        gets(bp);
+        cout<<"Destination pt:";
+        gets(dest);
+        cout<<"No of seats in first class & fare per ticket:";
+        cin>>c1>>c1fare;
+        cout<<"No of seats in second class & fare per ticket:";
+        cin>>c2>>c2fare;
+        cout<<"Date of travel:";
+        cin>>d>>m>>y;
+    }
+    void displaydetail()
+    {
+        cout<<tno<<"\t"<<tname<<"\t"<<bp<<"\t"<<dest<<"\t";
+        cout<<c1<<"\t"<<c1fare<<"\t"<<c2<<"\t"<<c2fare<<"\t";
+        cout<<d<<"-"<<m<<"-"<<y<<"\t"<<endl;
+    }
+};
+
