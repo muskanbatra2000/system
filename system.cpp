ceb35421ff7bf8b3e7168b8f9deee572d6a23907
Rakshit Kapoor <500067642@stu.upes.ac.in>
2019-04-25 23:45:32 +0530

    code addded

--- system.cpp
+++ system.cpp
@@ -68,4 +68,82 @@ public:
         cout<<d<<"-"<<m<<"-"<<y<<"\t"<<endl;
     }
 };
+class reser
+{
+public:
+    int pnr;
+    int tno;
+    char tname[100];
+    char bp[10];
+    char dest[100];
+    char pname[10][100];
+    int age[20];
+    char clas[10];
+    int nosr;
+    int i;
+    int d,m,y;
+    int con;
+
+
+    float amc;
+    void getresdet()
+    {
+        cout<<"Enter the details as follows\n";
+        cout<<"Train no:";
+        cin>>tno;
+        cout<<"Train name:";
+        gets(tname);
+        cout<<"Boarding point:";
+        gets(bp);
+        cout<<"Destination pt:";
+        gets(dest);
+        cout<<"No of seats required:";
+        cin>>nosr;
+        for(i=0; i<nosr ; i++)
+        {
+            cout<<"Passenger name:";
+            gets(pname[i]);
+            cout<<"Passenger age:";
+            cin>>age[i];
+        }
+        cout<<"Enter the class f-first class s-second class:";
+        gets(clas);
+        cout<<"Date of travel:";
+        cin>>d>>m>>y;
+        cout<<"Enter the concession category\n";
+        cout<<"1.Military\n2.Senior citizen\n";
+        cout<<"3.Children below 5 yrs\n4.None\n";
+        cin>>con;
+        cout<<"............END OF GETTING DETAILS............\n";
+    }
+    void displayresdet()
+    {
+        cout<<"...............................................\n";
+        cout<<"...............................................\n";
+        cout<<"Pnr no:"<<pnr;
+        cout<<"\nTrain no:"<<tno;
+        cout<<"\nTrain name:";
+        puts(tname);
+        cout<<"Boarding point:";
+        puts(bp);
+        cout<<"Destination pt:";
+        puts(dest);
+        cout<<"No of seats reserved:"<<nosr;
+        for(i=0; i<nosr; i++)
+        {
+            cout<<"Passenger name:";
+            puts(pname[i]);
+            cout<<"Passenger age:"<<age[i];
+        }
+
+        cout<<"\nYour class:";
+        puts(clas);
+        cout<<"\nDate of reservation:"<<d<<"-"<<m<<"-"<<y;
+        cout<<"\nYour concession category:"<<con;
+        cout<<"\nYou must pay:"<<amc<<endl;
+        cout<<"***********************************************\n";
+        cout<<".........END OF RESERVATION.................\n";
+        cout<<"***********************************************\n";
+    }
+};
 
