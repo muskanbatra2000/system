2b57e23ff85ba023704c5bfc3370bf94571539fa
Rakshit Kapoor <500067642@stu.upes.ac.in>
2019-04-25 23:46:35 +0530

    Code added

--- system.cpp
+++ system.cpp
@@ -146,4 +146,57 @@ public:
         cout<<"***********************************************\n";
     }
 };
+class canc
+{
+public:
+    int pnr;
+    int tno;
+    char tname[100];
+    char bp[10];
+    char dest[100];
+    char pname[10][100];
+    int age[20];
+    int i;
+    char clas[10];
+    int nosc;
+    int d,m,y;
+    float amr;
+    void getcancdet()
+    {
+        cout<<"Enter the details as follows\n";
+        cout<<"Pnr no:";
+        cin>>pnr;
+        cout<<"Date of cancellation:";
+        cin>>d>>m>>y;
+        cout<<"...........END OF GETTING DETAILS...........\n";
+    }
+    void displaycancdet()
+    {
+        cout<<"...........................................\n";
+        cout<<"...........................................\n";
+        cout<<"Pnr no:"<<pnr;
+        cout<<"\nTrain no:"<<tno;
+        cout<<"\nTrain name:";
+        puts(tname);
+        cout<<"Boarding point:";
+        puts(bp);
+
+        cout<<"Destination pt:";
+        puts(dest);
+        cout<<"\nYour class:";
+        puts(clas);
+        cout<<"no of seats to be cancelled:"<<nosc;
+        for(i=0; i<nosc; i++)
+        {
+            cout<<"Passenger name:";
+            puts(pname[i]);
+            cout<<"passenger age:"<<age[i];
+        }
+        cout<<"\nDate of cancellation:"<<d<<"-"<<m<<"-"<<y;
+        cout<<"\nYou can collect:"<<amr<<"rs"<<endl;
+        cout<<"*****************************************\n";
+        cout<<".........END OF CANCELLATION.............\n";
+        cout<<"*****************************************\n";
+    }
+};
 
